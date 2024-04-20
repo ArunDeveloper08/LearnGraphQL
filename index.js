045aeb6560b9730e70e09840b36a564a0d3b07a9
Arun <arunkanojiya781@gmail.com>
2024-04-20 14:49:59 +0530

    Revert "second commit"
    This reverts commit db355c7176fa366c489f009b861ade0a16065985.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function startServer() {
  
   app.use("/graphql", expressMiddleware(server));
 
-  const PORT =8000;
+  const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
